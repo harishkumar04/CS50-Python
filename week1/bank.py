e310aeda7c65eb696e2306c94eca45e459178ca9
R Harish Kumar <115402471+harishkumar04@users.noreply.github.com>
2025-08-08 07:08:31 +0000

    Added

---
+++ week1/bank.py
@@ -0,0 +1,15 @@
+def main():
+    g = input("Greeting: ").strip().lower()
+    gr = value(g)
+    print(f"${gr}")
+
+def value(gre):
+    if gre.startswith('hello'):
+        return 0
+    elif gre.gre.startswith('h'):
+        return 20
+    else:
+        return 100
+
+if __name__ == "__main__":
+    main()
